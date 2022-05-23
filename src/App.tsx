42bfc6d8baa25d92f1bfc97ab8fb22c76d755f66
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-05-23 23:01:10 +0200

    conditional rendering of WelcomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import './App.css';
 import WelcomePage from "./Components/WelcomePage"
 
 function App() {
+
+  const [hasAppStarted,setHasAppStarted] = React.useState(false)
+
+  function startApp() {
+    setHasAppStarted(prevHasAppStarted => !prevHasAppStarted)
+   // localStorage.setItem("welcomePageShown","shown") for later use
+
+  }
   return (
     <div >
-      <WelcomePage />
+      {!hasAppStarted && <WelcomePage handlePageChange={startApp}/>}
     </div>
   );
 }
--- src/Components/WelcomePage.tsx
+++ src/Components/WelcomePage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Typed from "typed.js"
 
-export default function WelcomePage(){
+export interface Props {
+    handlePageChange: () => void
+}
+
+export default function WelcomePage(props: Props){
     React.useEffect(() => {
 		const options: {
             strings: string[]
@@ -16,11 +20,13 @@ export default function WelcomePage(){
             strings: string[]
             startDelay: number
             typeSpeed: number
+            backSpeed: number
         }  = {
 			...options,
-			strings: ["The only place <strong>you need</strong> to maintain your habbits."],
+			strings: ["The only place <strong>you need</strong> to maintain your habbits.","The only place <strong>you need</strong> to become a better version of yourself."],
 			startDelay: 2200,
 			typeSpeed: 40,
+            backSpeed: 40
 		};
 
 		const typedHeader = new Typed(".header-container", options);
@@ -31,12 +37,13 @@ export default function WelcomePage(){
 			typedText.destroy();
 		};
 	}, []);
+
     return(
         <div className="flex justify-center items-center flex-wrap bg-linen h-screen text-center">
             <div>
                 <h1 className="header-container text-7xl w-full"></h1>
                 <p className="text-container text-2xl my-8"></p>
-                <button className="px-8 py-4  rounded-xl border-2 border-paleSilver">Begin your journey</button>
+                <button onClick={props.handlePageChange} className="px-8 py-4  rounded-xl border-2 border-paleSilver">Begin your journey</button>
             </div>
         </div>
     )
