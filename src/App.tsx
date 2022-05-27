3fa91a51304ade1b816a49ea5f4d07015ae81d3d
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-05-27 23:59:47 +0200

    Display username from localstorage and main Journal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "./App.css";
 import WelcomePage from "./Components/WelcomePage";
+import Journal from "./Components/Journal";
 
 function App() {
 	const [hasAppStarted, setHasAppStarted] = React.useState(false);
+	const username: string | null = localStorage.getItem("username") === null ? "Guest" : localStorage.getItem("username");
 
-	function startApp() {
+	function startApp(): void {
 		setHasAppStarted((prevHasAppStarted) => !prevHasAppStarted);
-		// localStorage.setItem("welcomePageShown","shown") for later use
+		localStorage.setItem("welcomePageShown", "shown");
 	}
 
 	return (
 		<div className="bg-linen">
 			{!hasAppStarted && <WelcomePage handlePageChange={startApp} />}
-			{hasAppStarted && <div className="h-screen">Welcome {localStorage.getItem("username")}</div>}
+			{hasAppStarted && (
+				<div className="h-screen p-10">
+					<div>Hi, {username}!</div>
+					<Journal />
+				</div>
+			)}
 		</div>
 	);
 }
---
+++ src/Components/Journal.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Journal() {
+	return (
+		<div className="flex justify-center align-bottom">
+			<h2 className="text-3xl">What have you accomplished today?</h2>
+		</div>
+	);
+}
