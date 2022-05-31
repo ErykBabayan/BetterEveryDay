6bda5a5a696899098902c97cd2d53ca26a15e619
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-05-31 19:23:34 +0200

    Entry html separated to custom component

---
+++ src/Components/Entry.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Entry() {
+	return (
+		<React.Fragment>
+			<h2 className="mt-24 text-2xl sm:text-3xl lg:text-5xl  text-center">What have you accomplished today?</h2>
+			<div className="w-full flex flex-wrap justify-center mt-12">
+				<input
+					className="bg-inherit p-2 rounded-lg border-2 text-center w-5/6 md:w-4/6 xl:w-3/6 border-paleSilver"
+					type="text"
+					placeholder="Title"
+				/>
+				<div className="w-full flex justify-center mt-2">
+					<textarea
+						placeholder="Description"
+						className="resize-none w-5/6 md:w-4/6 xl:w-3/6 bg-inherit p-2 rounded-lg border-2 text-center border-paleSilver"
+						maxLength={500}
+					></textarea>
+				</div>
+				<button className="px-8 py-4 my-8 w-5/6 md:w-4/6 xl:w-3/6 rounded-xl border-2 border-paleSilver hover:shadow-lg active:translate-y-0.5 active:shadow-xl">
+					Add
+				</button>
+			</div>
+			<hr className="w-5/6 xl:w-4/6 h-0.5 bg-paleSilver" />
+		</React.Fragment>
+	);
+}
--- src/Components/Journal.tsx
+++ src/Components/Journal.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Entry from "./Entry";
 
 export interface Props {}
 
 export default function Journal(props: Props) {
 	return (
-		<div className="flex justify-center align-bottom">
-			<h2 className="mt-24 text-2xl sm:text-3xl md:text-4xl  text-center">What have you accomplished today?</h2>
+		<div className="flex flex-wrap justify-center align-bottom">
+			<Entry />
 		</div>
 	);
 }
