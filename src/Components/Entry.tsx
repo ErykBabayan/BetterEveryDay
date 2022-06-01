1ea35473fec784bc2072aa24f5c32111b9f216b2
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-06-01 15:00:49 +0200

    Get userInput value to React state

--- src/Components/Entry.tsx
+++ src/Components/Entry.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 
-export default function Entry() {
+export interface Props {
+	getEntryData: (event: any) => void;
+}
+
+export default function Entry(props: Props) {
 	return (
 		<React.Fragment>
 			<h2 className="mt-24 text-2xl sm:text-3xl lg:text-5xl  text-center">What have you accomplished today?</h2>
 			<div className="w-full flex flex-wrap justify-center mt-12">
 				<input
+					onChange={props.getEntryData}
+					id="entryTopic"
 					className="bg-inherit p-2 rounded-lg border-2 text-center w-5/6 md:w-4/6 xl:w-3/6 border-paleSilver"
 					type="text"
 					placeholder="Title"
 				/>
 				<div className="w-full flex justify-center mt-2">
 					<textarea
+						onChange={props.getEntryData}
+						id="entryDescription"
 						placeholder="Description"
 						className="resize-none w-5/6 md:w-4/6 xl:w-3/6 bg-inherit p-2 rounded-lg border-2 text-center border-paleSilver"
 						maxLength={500}
--- src/Components/Journal.tsx
+++ src/Components/Journal.tsx
@@ -4,9 +4,16 @@ import Entry from "./Entry";
 export interface Props {}
 
 export default function Journal(props: Props) {
+	const [userTopicInput, setUserTopicInput] = React.useState("");
+	const [userDescriptionInput, setUserDescriptionInput] = React.useState("");
+
+	function getEntryData(event: any): void {
+		event.target.id === "entryTopic" ? setUserTopicInput(event.target.value) : setUserDescriptionInput(event.target.value);
+	}
+
 	return (
 		<div className="flex flex-wrap justify-center align-bottom">
-			<Entry />
+			<Entry getEntryData={getEntryData} />
 		</div>
 	);
 }
