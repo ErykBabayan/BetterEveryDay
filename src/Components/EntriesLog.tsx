006886ceb5f3e63db0bc3003bab8fa8da3a47d12
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-06-13 07:31:45 +0200

    EntryLog custom component

--- src/Components/EntriesLog.tsx
+++ src/Components/EntriesLog.tsx
@@ -1,3 +1,9 @@
 import React from "react";
 
-export default function EntriesLog() {}
+export default function EntriesLog() {
+	return (
+		<React.Fragment>
+			<h3 className="w-full text-center text-gray-500 text-lg sm:text-2xl lg:text-4xl ">Currently tracking : 100 days of code </h3>
+		</React.Fragment>
+	);
+}
