18a080cadc2cda9819449de486593276e40fa210
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-06-13 07:32:20 +0200

    function to store all entries in 1 array

--- src/Components/Journal.tsx
+++ src/Components/Journal.tsx
@@ -1,19 +1,36 @@
 import React from "react";
 import UserInput from "./UserInput";
-
+import EntriesLog from "./EntriesLog";
 export interface Props {}
 
 export default function Journal(props: Props) {
-	const [userTopicInput, setUserTopicInput] = React.useState("");
-	const [userDescriptionInput, setUserDescriptionInput] = React.useState("");
-
+	const [userTopicInput, setUserTopicInput] = React.useState();
+	const [userDescriptionInput, setUserDescriptionInput] = React.useState();
+	const [allEntries, setAllEntries]: any = React.useState([]);
 	function getEntryData(event: any): void {
 		event.target.id === "entryTopic" ? setUserTopicInput(event.target.value) : setUserDescriptionInput(event.target.value);
 	}
 
+	function addEntry(): void {
+		const currentEntries: object[] = [];
+		const inputEntry: {
+			topic: string | undefined;
+			description: string | undefined;
+		} = {
+			topic: userTopicInput,
+			description: userDescriptionInput,
+		};
+		currentEntries.push(inputEntry);
+		allEntries.forEach((entry) => {
+			currentEntries.push(entry);
+		});
+		console.log(currentEntries);
+		setAllEntries(currentEntries);
+	}
 	return (
-		<div className="flex flex-wrap justify-center align-bottom">
-			<UserInput getEntryData={getEntryData} />
+		<div className="flex flex-wrap justify-center">
+			<UserInput getEntryData={getEntryData} addEntry={addEntry} />
+			<EntriesLog />
 		</div>
 	);
 }
